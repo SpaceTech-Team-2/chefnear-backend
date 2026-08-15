bbf16cb04e18d51379d38e75933a717a6a52dbca
Mazen Elnems <mazenelnems22@gmail.com>
2026-08-15 18:24:59 +0300

    fix(Payment): add RefundRequestAt column to Payments to track refund flow

---
+++ src/ChefNear.Infrastructure/Migrations/20260815152357_AddRefundRequestedAtColumnInPayments.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChefNear.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefundRequestedAtColumnInPayments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefundRequestedAt",
+                table: "Payments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefundRequestedAt",
+                table: "Payments");
+        }
+    }
+}
